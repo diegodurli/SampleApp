be0d84c55eb3e40161f16778d90f29233c7d75ae
Diego Durli <dgodurli@gmail.com>
2012-02-21 16:26:43 -0200

    Made a working feed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -200,9 +200,15 @@ describe User do
       end
       
       it "should not include a different user's microposts" do
-        mp3 = Factory(:micropost,
-                       user: Factory(:user, email: Factory.next(:email)))
+        mp3 = Factory(:micropost, user: Factory(:user, email: Factory.next(:email)))
         @user.feed.should_not include(mp3)
+      end
+      
+      it "should include the microposts of followed users" do
+        followed = Factory(:user, email: Factory.next(:email))        
+        mp4 = Factory(:micropost, user: followed)
+        @user.follow!(followed)
+        @user.feed.should include(mp4)
       end
     end
   end
