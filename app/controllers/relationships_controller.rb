c0308863ec61fad37b32a963053e42c3487df4dd
Diego Durli <dgodurli@gmail.com>
2012-02-21 15:19:48 -0200

    Working follow & unfollow buttons with Ajax

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,16 @@
+class RelationshipsController < ApplicationController
+  respond_to :html, :js
+  before_filter :authenticate
+
+  def create
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
+    respond_with @user
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    respond_with @user
+  end
+end
